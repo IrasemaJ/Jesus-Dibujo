e0b24827cf50803ae71bb163c1a5a4c09ad2bacf
IrasemaJ <IrasemaJ@users.noreply.github.com>
2022-12-30 16:20:03 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,13 +7,12 @@ function setup() {
   background("white");
 
   //ALUMNO HACE
-  canvas.mouseReleased(classifyCanvas);
-  synth = window.speechSynthesis;
+  
 }
 
 //ALUMNO REALIZA
 function preload() {
-  classifier = ml5.imageClassifier('DoodleNet');
+  
 }
 
 function clearCanvas() {
@@ -24,36 +23,17 @@ function clearCanvas() {
 //ALUMNO REALIZA
 function draw() {
 
-  // GROSOR DE LINEA
-  strokeWeight(13);
-  // COLOR NEGRO.
-  stroke(0);
-  // MOUSE PRESIONADO 
-  if (mouseIsPressed) {
-    line(pmouseX, pmouseY, mouseX, mouseY);
-    //VARIABLE DE P5.JS pmouseX-pmouseY CORDENADAS ANTERIORES
-    //VARIABLE DE P5.JS mouseX-mouseY CORDENADAS ACTUALES
-  }
+  
 }
 
 //ALUMNO REALIZA
 function classifyCanvas() {
-  classifier.classify(canvas, gotResult);
+  
 }
 
 //ALUMNO REALIZA
 function gotResult(error, results) {
-  if (error) {
-    console.error(error);
-  }
-  console.log(results);
-  document.getElementById('label').innerHTML = 'Etiqueta: ' + results[0].label;
-
-  document.getElementById('confidence').innerHTML = 'Confianza: ' + Math.round(results[0].confidence * 100) + '%';
-
-  utterThis = new SpeechSynthesisUtterance(results[0].label);
-  utterThis.lang = 'en-US';
-  synth.speak(utterThis);
+  
 }
 
 
